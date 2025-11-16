b697e916a012408e15d7d64393ba30287a3913f4
Hema Sai Reddy <158816242+BunnyHemaSaiReddy@users.noreply.github.com>
2025-11-16 12:32:28 +0530

    Update scriptl.js

--- scriptl.js
+++ scriptl.js
@@ -6,7 +6,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('http://bunny-blooddonation.onrender.com/api/login', {
+        const response = await fetch('https://bunny-blooddonation.onrender.com/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -35,4 +35,5 @@ document.getElementById('loginForm').addEventListener('submit', async function (
         alert('An error occurred. Please try again.');
     }
 });
+
 
